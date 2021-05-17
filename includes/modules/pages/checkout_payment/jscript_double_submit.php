1b8330367656626e2f589d833fe8f9acc6166e43
Nativefaith40 <403281776@qq.com>
2021-05-17 13:22:41 +0800

    delete

--- includes/modules/pages/checkout_payment/jscript_double_submit.php
+++
@@ -1,6 +0,0 @@
-<?php 
-  // For payment modules using AJAX for security, confirmation page JS is 
-  // not loaded.  So load the relevant file here. 
-  if (PADSS_AJAX_CHECKOUT == '1') { 
-    require(DIR_WS_MODULES . '/pages/checkout_confirmation/jscript_double_submit.php'); 
-  }
